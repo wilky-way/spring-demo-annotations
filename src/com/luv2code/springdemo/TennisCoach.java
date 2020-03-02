31d6631fdab04b7f16aa72b4b7d0156ba3af6af5
Alex Wilkinson <awilk901@gmail.com>
2020-03-02 10:16:32 -0600

    Use default bean id instead of explicit

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component //uses default bean id "tennisCoach"
 public class TennisCoach implements Coach {
 
     @Override
