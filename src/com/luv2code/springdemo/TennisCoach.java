b080c8afc1a8dd4355a2a9982ab9e5896bba35bd
Alex Wilkinson <awilk901@gmail.com>
2020-03-07 11:41:29 -0600

    Setter Injection Demo

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -8,10 +8,24 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    //define default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define a setter method
+    @Autowired
+    public void setFortuneService(FortuneService theFortuneService){
+        System.out.println(">> TennisCoach: inside setFortuneService");
+        fortuneService = theFortuneService;
+    }
+
+    /*
     @Autowired // Spring scans for component that implements FortuneService interface... ex. HappyFortuneService
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+    */
 
     @Override
     public String getDailyWorkout() {
