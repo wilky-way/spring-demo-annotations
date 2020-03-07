ae05766be524e351d73e35f67b310b9d75e7e864
Alex Wilkinson <awilk901@gmail.com>
2020-03-07 13:08:36 -0600

    Bean Lifecycle method annotations (doesnt work w/ java 9+ by default so commented out)

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class TennisCoach implements Coach {
     //@Autowired // field injection
     private FortuneService fortuneService;
@@ -16,6 +16,17 @@ public class TennisCoach implements Coach {
         System.out.println(">> TennisCoach: inside default constructor");
     }
 
+    // define my init method
+    //@PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside doMyStartupStuff()");
+    }
+
+    // define my destroy method
+    // @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">> TennisCoach: inside doMyCleanupStuff()");
+    }
     /*
     // method injection
     @Autowired
