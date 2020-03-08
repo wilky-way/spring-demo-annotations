04e016c33d972af1a087054d7c8f3f6db928cafe
Alex Wilkinson <awilk901@gmail.com>
2020-03-08 14:20:33 -0500

    Defining Spring beans with Java code, no @Component, good for 3rd party classes

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,11 +1,22 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
 
+    // define bean for our sad fortune service
+    @Bean
+    FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
 
+    // define bean for swim coach and inject dependency
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
